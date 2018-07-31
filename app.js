4d1812fb7ebedadb8b6fdb1b3dbff563a48fd455
Victor Bezerra <victorbezerr@hotmail.com>
2018-07-31 14:59:54 -0300

    função de insert e select

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
 const Sequelize = require('sequelize');
+const bodyParser = require('body-parser');
 
 app.set('view engine', 'pug');
+app.use(bodyParser.urlencoded({entended: false}));
+app.use(bodyParser.json());
+
 // criando a conexão com o mysql
 var sequelize = new Sequelize('mysql://root@localhost:3306/expressjm');
 
@@ -25,6 +29,20 @@ var User = sequelize.define('usuarios',{
 //     });
 //   });
 
+// Select com todos os dados da tabela
+app.get('/users',(req, res)=>{
+  User.findAll()
+    .then((result)=>{
+      res.render('users',{
+        message: 'Lista de usuários',
+        data: result
+      });
+    })
+    .catch((err)=>{
+      console.log("Error: ", err);
+    });
+});
+
 // Select com um único usuário através do id
 app.get('/users/:id', (req,res)=>{
   // User.findOne({
@@ -45,18 +63,23 @@ app.get('/users/:id', (req,res)=>{
     });
 });
 
-// Select com todos os dados da tabela
-app.get('/users',(req, res)=>{
-  User.findAll()
-    .then((result)=>{
-      res.render('users',{
-        message: 'Lista de usuários',
-        data: result
+// Adicionando usuário na tabela
+app.get('/users/create', (req, res)=>{
+  res.render('newUsers',{
+    message: 'Criando um usuário'
+  });
+});
+
+app.post('/users/create',(req,res)=>{
+  User.create(req.body)
+    .then(()=>{
+      res.render('newUsers',{
+        message: 'Criando um usuário'
       });
     })
     .catch((err)=>{
-      console.log("Error: ", err);
-    });
+      console.log("ERRO: ", err);
+    })
 });
 
 app.get('/', (req, res)=>{
